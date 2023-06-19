ec4da166abd0612f872ce26dff27ef533c0ae8f1
DAJA <33202282+DAJAKMPM@users.noreply.github.com>
2023-06-19 20:06:27 +0800

    wip: on-going

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { appSetup } from './config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appSetup],
+      envFilePath: `${process.cwd()}/.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRoot(appSetup().database),
+    UsersModule,
+    ReportsModule,
+  ],
 })
 export class AppModule {}
---
+++ src/config/database.ts
@@ -0,0 +1,57 @@
+import type { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as process from 'process';
+
+const env = process.env;
+
+const getDatabaseConfig = () => {
+  const dbConfig = {
+    synchronize: true,
+    migrations: ['migrations/*.js'],
+    cli: {
+      migrationsDir: 'migrations',
+    },
+  };
+
+  switch (process.env.NODE_ENV) {
+    case 'development':
+      Object.assign(dbConfig, {
+        type: 'postgres',
+        host: env.DB_HOST,
+        port: +env.DB_PORT || 5432,
+        username: env.DB_USER,
+        password: env.DB_PASSWORD,
+        database: env.DB_DATABASE,
+        keepConnectionAlive: true,
+        entities: ['**/*.entity.js'],
+      });
+      break;
+    case 'test':
+      Object.assign(dbConfig, {
+        type: 'sqlite',
+        database: 'test.sqlite',
+        entities: ['**/*.entity.ts'],
+        migrationsRun: true,
+      });
+      break;
+    case 'production':
+      Object.assign(dbConfig, {
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        migrationsRun: true,
+        entities: ['**/*.entity.js'],
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      });
+      break;
+    default:
+      throw new Error('unknown environment');
+  }
+
+  return dbConfig;
+};
+
+export const appSetup = () => ({
+  port: Number(env.PORT),
+  database: getDatabaseConfig() as TypeOrmModuleOptions,
+});
